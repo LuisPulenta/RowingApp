4982c3972f677c06b8ba2a559d0f79f697910783
LuisPulenta <luisalbertonu@gmail.com>
2022-03-24 12:41:19 -0300

    VehiculosKilometrajeController

---
+++ GenericApp.Common/Requests/VehiculosKilometrajeRequest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GenericApp.Common.Requests
+{
+    public class VehiculosKilometrajeRequest
+    {
+        public DateTime Fecha { get; set; }
+        public string Equipo { get; set; }
+        public int? KILINI { get; set; }
+        public int? KILFIN { get; set; }
+        public int? HORSAL { get; set; }
+        public int? HORLLE { get; set; }
+        public byte? CODSUC { get; set; }
+        public int? NRODEOT { get; set; }
+        public string CAMBIO { get; set; }
+        public byte? PROCESADO { get; set; }
+        public DateTime KMFECHAANTERIOR { get; set; }
+        public int? NOPROMEDIAR { get; set; }
+        public DateTime FECHAALTA { get; set; }
+    }
+}
---
+++ GenericApp.Web/Controllers/API/VehiculosKilometrajeController.cs
@@ -0,0 +1,49 @@
+﻿using GenericApp.Common.Requests;
+using GenericApp.Web.Data;
+using GenericApp.Web.Data.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace GenericApp.Web.Controllers.API
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class VehiculosKilometrajeController : ControllerBase
+    {
+        private readonly DataContext2 _context;
+        public VehiculosKilometrajeController(DataContext2 context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostVehiculosKilometraje([FromBody] VehiculosKilometrajeRequest request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            VehiculosKilometraje kilometraje = new VehiculosKilometraje
+            {
+                CAMBIO = request.CAMBIO,
+                FECHAALTA = request.FECHAALTA,
+                HORSAL = request.HORSAL,
+                KMFECHAANTERIOR = request.KMFECHAANTERIOR,
+                NOPROMEDIAR = request.NOPROMEDIAR,
+                PROCESADO = request.PROCESADO,
+                CODSUC = request.CODSUC,
+                Equipo = request.Equipo,
+                Fecha = request.Fecha,
+                HORLLE = request.HORLLE,
+                KILFIN = request.KILFIN,
+                KILINI = request.KILINI,
+                NRODEOT = request.NRODEOT,
+                Orden = 0,
+            };
+            _context.VehiculosKilometrajes.Add(kilometraje);
+            await _context.SaveChangesAsync();
+            return Ok(kilometraje);
+        }
+    }
+}
