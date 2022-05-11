520c11718ad9c49b46fdd3e043f4661f0afcf6b9
LuisPulenta <luisalbertonu@gmail.com>
2022-05-11 11:53:16 -0300

    InspeccionesController

--- GenericApp.Web/Data/Entities/SHInspeccionDetall.cs
+++ GenericApp.Web/Data/Entities/SHInspeccionDetall.cs
@@ -6,13 +6,13 @@ namespace GenericApp.Web.Data.Entities
     public class SHInspeccionDetall
     {
         [Key]
-        public int IDREGISTRO { get; set; }
-        public int CLAVEINSPECCIONCAB { get; set; }
-        public int IDCLIENTE { get; set; }
-        public int IDGRUPOFORMULARIO { get; set; }
-        public string DETALLEF { get; set; }
-        public string DESCRIPCION { get; set; }
-        public int PONDERACIONPUNTOS { get; set; }
-        public string CUMPLE { get; set; }
+        public int IDRegistro { get; set; }
+        public int InspeccionCab { get; set; }
+        public int IdCliente { get; set; }
+        public int IDGrupoFormulario { get; set; }
+        public string DetalleF { get; set; }
+        public string Descripcion { get; set; }
+        public int PonderacionPuntos { get; set; }
+        public string Cumple { get; set; }
     }
 }
