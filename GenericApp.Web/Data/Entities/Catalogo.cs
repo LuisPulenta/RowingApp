4de7fcee6d920ba4c2526780af862fe571244d41
LuisPulenta <luisalbertonu@gmail.com>
2022-08-24 17:23:11 -0300

    Corrección en CatalogosController

--- GenericApp.Web/Data/Entities/Catalogo.cs
+++ GenericApp.Web/Data/Entities/Catalogo.cs
@@ -9,9 +9,9 @@ namespace GenericApp.Web.Data.Entities
         public string catCodigo { get; set; }
         public string CodigoSap { get; set; }
         public string catCatalogo { get; set; }
-        public int VerEnReclamosApp { get; set; }
-        public int VerRequerimientosAPP { get; set; }
-        public int VerRequerimientosEPP { get; set; }
+        public int? VerEnReclamosApp { get; set; }
+        public int? VerRequerimientosAPP { get; set; }
+        public int? VerRequerimientosEPP { get; set; }
         public string Modulo { get; set; }
     }
 }
