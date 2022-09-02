222993c0b809ba9807bbd21903759816d8d4b265
LuisPulenta <luisalbertonu@gmail.com>
2022-09-02 15:18:01 -0300

    GetSubcontratistas

--- GenericApp.Web/Controllers/API/SubcontratistaController.cs
+++ GenericApp.Web/Controllers/API/SubcontratistaController.cs
@@ -1,6 +1,13 @@
-﻿using GenericApp.Web.Data;
+﻿using GenericApp.Common.Helpers;
+using GenericApp.Common.Requests;
+using GenericApp.Web.Data;
+using GenericApp.Web.Data.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace GenericApp.Web.Controllers.API
 {
@@ -16,17 +23,26 @@ namespace GenericApp.Web.Controllers.API
             _dataContext = dataContext;
             _dataContext2 = dataContext2;
         }
-
-
+     
 
         [HttpPost]
-        [Route("GetSubcontratistas/{ProyectoModulo}")]
-        public IActionResult GetObrasReclamos(string ProyectoModulo)
+        [Route("GetSubcontratistas/{proyectomodulo}")]
+        public async Task<IActionResult> GetSubcontratistas(string proyectoModulo)
         {
-            return Ok(_dataContext.Subcontratistas
-                .Where(o => o.MODULO == ProyectoModulo && o.subDeshabilitado==0)
-                .OrderBy(o => o.subSubcontratista)
-                );
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            var subContratistas = await _dataContext.Subcontratistas
+            .Where(o => (o.MODULO == proyectoModulo || o.MODULO == "Compartido") && o.subDeshabilitado==0)
+            .OrderBy(o => o.subSubcontratista)
+            .ToListAsync();
+            if (subContratistas == null)
+            {
+                return BadRequest("No hay SubContratistas.");
+            }
+            return Ok(subContratistas);
         }
     }
 }
