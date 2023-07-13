24e9a86e1e295e8d15470f15221b94be7495facb
LuisPulenta <luisalbertonu@gmail.com>
2023-07-13 11:09:12 -0300

    ObrasController PutDatosObra

--- GenericApp.Web/Controllers/API/ObrasController.cs
+++ GenericApp.Web/Controllers/API/ObrasController.cs
@@ -19,6 +19,42 @@ namespace GenericApp.Web.Controllers.API
             _filesHelper = filesHelper;
         }
 
+        [HttpPut]
+        [Route("PutDatosObra/{id}")]
+        public async Task<IActionResult> PutDatosObra([FromRoute] int id, [FromBody] ObraDatosRequest request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != request.NroObra)
+            {
+                return BadRequest();
+            }
+
+            var oldObra = await _dataContext.Obras.FindAsync(request.NroObra);
+            if (oldObra == null)
+            {
+                return BadRequest("La Obra no existe.");
+            }
+
+            oldObra.POSX = request.POSX;
+            oldObra.POSY = request.POSY;
+            oldObra.Direccion = request.Direccion;
+            oldObra.TextoLocalizacion = request.TextoLocalizacion;
+            oldObra.TextoClase = request.TextoClase;
+            oldObra.TextoTipo = request.TextoTipo;
+            oldObra.TextoComponente = request.TextoComponente;
+            oldObra.CodigoDiametro = request.CodigoDiametro;
+            oldObra.Motivo = request.Motivo;
+            oldObra.Planos = request.Planos;
+
+            _dataContext.Obras.Update(oldObra);
+            await _dataContext.SaveChangesAsync();
+            return Ok();
+        }
+
         [HttpPut("{id}")]
         public async Task<IActionResult> PutFechaCierreElectrico([FromRoute] int id, [FromBody] ObraFechaCierreElectricoRequest request)
         {
