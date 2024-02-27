8b39fc5d3883e59b075a0cd19657b5dbef7805c1
LuisPulenta <luisalbertonu@gmail.com>
2024-02-27 10:33:47 -0300

    MovimientosController

---
+++ GenericApp.Common/Requests/MovimientoRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace GenericApp.Common.Requests
+{
+    public class MovimientoRequest
+    {
+        public int NroMovimiento { get; set; }
+        public byte[] ImageArray { get; set; }
+    }
+}
--- GenericApp.Web/Controllers/API/MovimientosController.cs
+++ GenericApp.Web/Controllers/API/MovimientosController.cs
@@ -1,7 +1,11 @@
-﻿using GenericApp.Web.Data;
+﻿using GenericApp.Common.Helpers;
+using GenericApp.Common.Requests;
+using GenericApp.Web.Data;
+using GenericApp.Web.Data.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -12,10 +16,12 @@ namespace GenericApp.Web.Controllers.API
     public class MovimientosController : ControllerBase
     {
         private readonly DataContext2 _dataContext;
+        private readonly IFilesHelper _filesHelper;
 
-        public MovimientosController(DataContext2 dataContext)
+        public MovimientosController(DataContext2 dataContext, IFilesHelper filesHelper)
         {
             _dataContext = dataContext;
+            _filesHelper = filesHelper;
         }
 
         //-----------------------------------------------------------------------------------
@@ -44,6 +50,47 @@ namespace GenericApp.Web.Controllers.API
                 return BadRequest("No hay Movimientos.");
             }
             return Ok(movimientos);
+        }
+
+        //-----------------------------------------------------------------------------------
+        [HttpPost]
+        [Route("PostMovimiento")]
+        public async Task<IActionResult> PostMovimiento([FromBody] MovimientoRequest request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            //Foto
+            string imageUrl = string.Empty;
+            if (request.ImageArray != null && request.ImageArray.Length > 0)
+            {
+                var stream = new MemoryStream(request.ImageArray);
+                var guid = Guid.NewGuid().ToString();
+                var file = $"{guid}.jpg";
+                var folder = "wwwroot\\images\\Remitos";
+                var fullPath = $"~/images/Remitos/{file}";
+                var response = _filesHelper.UploadPhoto(stream, folder, file);
+
+                if (response)
+                {
+                    imageUrl = fullPath;
+                }
+            }
+
+            Movimiento oldMovimiento = await _dataContext.Movimientos.FirstOrDefaultAsync(o => o.NroMovimiento == request.NroMovimiento);
+
+            if (oldMovimiento == null)
+            {
+                return BadRequest("El Movimiento no existe.");
+            }
+
+            oldMovimiento.LinkRemito = imageUrl;
+
+            _dataContext.Movimientos.Update(oldMovimiento);
+            await _dataContext.SaveChangesAsync();
+            return Ok();
         }
     }
 }
