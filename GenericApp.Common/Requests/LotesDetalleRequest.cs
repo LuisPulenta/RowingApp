a77bb5703a0d9cb8e07cc1895a9caf54828111cc
LuisPulenta <luisalbertonu@gmail.com>
2024-05-22 13:58:59 -0300

    LotesDetalleRequest

--- GenericApp.Common/Requests/LotesDetalleRequest.cs
+++ GenericApp.Common/Requests/LotesDetalleRequest.cs
@@ -4,7 +4,7 @@
     {
         public int MARCAR { get; set; }
         public int NROREGISTRO { get; set; }
-        public int IDInstalacionesEquipos { get; set; }
+        public int? IDInstalacionesEquipos { get; set; }
 
     }
 }
