9c40e4c9c17459639c1fefa1aa1e9545d5a06408
Luis Núñez <luisalbertonu@gmail.com>
2025-07-31 19:13:16 -0300

    SendMailWithPdf

--- GenericApp.Web/Helpers/IMailHelper.cs
+++ GenericApp.Web/Helpers/IMailHelper.cs
@@ -1,9 +1,12 @@
 ﻿using GenericApp.Common.Responses;
+using System.Threading.Tasks;
 
 namespace GenericApp.Web.Helpers
 {
     public interface IMailHelper
     {
         Response SendMail(string to, string subject, string body);
+
+        Task<Response> SendMailWithPdf(string to, string subject, string body, string fileUrl);
     }
 }
--- GenericApp.Web/Helpers/MailHelper.cs
+++ GenericApp.Web/Helpers/MailHelper.cs
@@ -5,6 +5,9 @@ using GenericApp.Common.Responses;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.IO;
 
 namespace GenericApp.Web.Helpers
 {
@@ -42,6 +45,71 @@ namespace GenericApp.Web.Helpers
                     HtmlBody = body
 
                 };
+                message.Body = bodyBuilder.ToMessageBody();
+
+                using (SmtpClient client = new SmtpClient())
+                {
+                    client.Connect(smtp, int.Parse(port), true);
+                    client.Authenticate(from, password);
+                    client.Send(message);
+                    client.Disconnect(true);
+                }
+
+                return new Response { IsSuccess = true };
+
+            }
+            catch (Exception ex)
+            {
+                return new Response
+                {
+                    IsSuccess = false,
+                    Message = ex.Message,
+                    Result = ex
+                };
+            }
+        }
+
+        public async Task<Response> SendMailWithPdf (string to, string subject, string body, string fileUrl)
+        {
+            try
+            {
+                string from = _configuration["Mail:From"];
+                string smtp = _configuration["Mail:Smtp"];
+                string port = _configuration["Mail:Port"];
+                string password = _configuration["Mail:Password"];
+
+                List<string> listaTO = to.Split(',').ToList();
+
+                MimeMessage message = new MimeMessage();
+                message.From.Add(new MailboxAddress(from));
+                foreach (var email in listaTO)
+                {
+                    message.To.Add(new MailboxAddress(email));
+                }
+
+                message.Subject = subject;
+
+                BodyBuilder bodyBuilder = new BodyBuilder
+                {
+                    HtmlBody = body
+
+                };
+
+                if (!string.IsNullOrEmpty(fileUrl))
+                {
+                    using (HttpClient client = new HttpClient())
+                    {
+                        var fileBytes = await client.GetByteArrayAsync(fileUrl);
+                        var fileName = Path.GetFileName(fileUrl);
+
+                        // Crear un ContentType para el archivo PDF
+                        var contentType = new MimeKit.ContentType("application", "pdf");
+
+                        // Agregar el archivo PDF como adjunto
+                        bodyBuilder.Attachments.Add(fileName, fileBytes, contentType);
+                    }
+                }
+
                 message.Body = bodyBuilder.ToMessageBody();
 
                 using (SmtpClient client = new SmtpClient())
