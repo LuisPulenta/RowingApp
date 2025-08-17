8faaa0ee9a8e20e35e2b1d92407114d0f60c7d82
Luis Núñez <luisalbertonu@gmail.com>
2025-08-17 10:40:40 -0300

    Cambio

--- Web/Models/Request/Vehiculo2Request.cs
+++ Web/Models/Request/Vehiculo2Request.cs
@@ -1,11 +1,10 @@
-﻿using System;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 namespace RowingApp.Common.Requests
 {
     public class Vehiculo2Request
     {
         [Required]
-        public Int16 Id { get; set; }
+        public int Id { get; set; }
 
         public int? KMHSACTUAL { get; set; }
     }
